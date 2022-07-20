52c778cb41ed4e721a9a7e4b8688aab1f4b5c4db
Leo Wandersleb <leo@leowandersleb.de>
2022-07-20 01:54:38 -0400

    filter by follows by degree of pubkey

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,14 @@ window.addEventListener('load', () => {
   setupWs('wss://relay.nostr.info/')
   setupWs('wss://nostr-pub.wellorder.net')
   setupWs('wss://relay.damus.io')
-  window.output = document.getElementById("output")
+  setupWs('wss://nostr.rocks')
   window.kindFilter = document.getElementById("kind-filter")
+  window.pubkeyFilter = document.getElementById("pubkey-filter")
+  window.degreeFilter = document.getElementById("degree-filter")
+  window.output = document.getElementById("output")
   setInterval(() => {
     if (dirty) {
-      // dedupe
-      received = [...new Map(received.map(x => [x.id, x])).values()]
-      received = received.sort( (a,b) => b.created_at - a.created_at )
-      received = received.slice(0, LIMIT)
-      const kind = kindFilter.value
-      if (kind === 'all') {
-        filtered = received
-      } else if (kind === 'unknown') {
-        const knownKinds = [0,1,2,3,4,5,6,7,60]
-        filtered = received.filter(ev => !knownKinds.includes(ev.kind))
-      } else {
-        filtered = received.filter(it => it.kind == kind)
-      }
-      output.innerHTML = filtered.map(it => eventBadge(it)).join('<br>')
-      dirty = false
+      update()
     }
   }, 1000)
 })
@@ -29,6 +18,72 @@ const LIMIT = 2000 // how many events to show
 var received = []
 var dirty = true
 const meta = {}
+const follows = {}
+
+function update() {
+  // dedupe
+  received = [...new Map(received.map(x => [x.id, x])).values()]
+  // newest first
+  received = received.sort( (a,b) => b.created_at - a.created_at )
+  // clip to only LIMIT events
+  received = received.slice(0, LIMIT)
+  
+  const kindFiltered = filterByKind()
+  const filtered = filterByPubkey(kindFiltered)
+  
+  output.innerHTML = `${filtered.length}/${LIMIT} Events:<br>`
+    + filtered.map(it => eventBadge(it)).join('<br>')
+  
+  if (pubkeyFilter.value) {
+    degreeFilter.removeAttribute('disabled')
+  } else {
+    degreeFilter.setAttribute('disabled', '')
+  }
+  dirty = false
+}
+
+function filterByPubkey(list) {
+  const pubkey = pubkeyFilter.value
+  const degree = degreeFilter.value
+  if (!pubkey) {
+    return list
+  }
+  if (pubkey.length != 64) {
+    return []
+  }
+  const followsByDegree = [new Set()]
+  followsByDegree[0].add(pubkey)
+  for (n=1; n<= degree; n++) {
+    // determine the n-th degree of follows
+    const f = new Set()
+    followsByDegree[n - 1]
+      .forEach(it => {
+        (follows[it] || [])
+          .forEach(x => f.add(x))
+        })
+    for (i=0; i<n; i++) {
+      // store each pubkey only in the lowest degree
+      followsByDegree[i].forEach(it => f.delete(it))
+    }
+    followsByDegree[n] = f
+  }
+  return list.map(it => {
+    it.degree = followsByDegree.findIndex(x => x.has(it.pubkey))
+    return it
+  }).filter(e => e.degree >= 0)
+}
+
+function filterByKind() {
+  const kind = kindFilter.value
+  if (kind === 'all') {
+    return received
+  } else if (kind === 'unknown') {
+    const knownKinds = [0,1,2,3,4,5,6,7,60]
+    return received.filter(ev => !knownKinds.includes(ev.kind))
+  } else {
+    return received.filter(it => it.kind == kind)
+  }
+}
 
 function nameFromPubkey(pubkey) {
   const m = meta[pubkey]
@@ -37,34 +92,12 @@ function nameFromPubkey(pubkey) {
   return `<span class="meta"><img src="${img}">&nbsp;${name}</span>`
 }
 
-Number.prototype.pad = function(size) {
-    var s = String(this);
-    while (s.length < (size || 2)) {s = "0" + s;}
-    return s;
-}
-
-function timeFormat(ts) {
-  let d = new Date(ts * 1000)
-  return `${d.getYear()-100}-${(d.getMonth()+1).pad(2)}-${(d.getDate()+1).pad(2)} ` +
-         `${d.getHours().pad(2)}:${d.getMinutes().pad(2)}:${d.getSeconds().pad(2)}`
-}
-
-function escapeHTML(str){
-  var p = document.createElement("p");
-  p.appendChild(document.createTextNode(str));
-  return p.innerHTML;
-}
-
-function guessClient(event) {
-  if (event.tags.find(i => i[0] === 'client' && i[1] === 'more-speech')) {
-    return '<a href="https://github.com/unclebob/more-speech">more-speech</a>'
-  }
-  return null
-}
-
 function eventBadge(event) {
+  const degree = pubkeyFilter.value
+    ? `(D=${event.degree}) `
+    : ''
   const from = nameFromPubkey(event.pubkey)
-  var badge = `<span class="event kind-${event.kind}">${timeFormat(event.created_at)} ${from} `
+  var badge = `<span class="event kind-${event.kind}">${timeFormat(event.created_at)}${degree} ${from} `
   switch (event.kind) {
     case 0: {
         badge += `Updated metadata.`
@@ -131,6 +164,11 @@ function setupWs(url) {
             console.log(`Should "${escapeHTML(event.content)}" be valid JSON?`)
           }
         }
+      } if (arr[1] == "follows") {
+        const event = arr[2]
+        if (event.kind === 3) {
+          follows[event.pubkey] = event.tags.filter(it => it[0] === "p").map(it => it[1])
+        }
       } else if (arr[1] === "main") {
         const event = arr[2]
         received.unshift(event)
@@ -139,6 +177,8 @@ function setupWs(url) {
     } else if (arr[0] === 'EOSE') {
       if (arr[1] === 'meta') {
         ws.send('["CLOSE","meta"]')
+      } else if (arr[1] === 'follows') {
+        ws.send('["CLOSE","follows"]')
       }
     }
   }
@@ -150,5 +190,31 @@ function setupWs(url) {
   ws.onopen = event => {
     ws.send(`["REQ","main",{"limit":${LIMIT},"before":${(new Date().getTime() / 1000 + 60 * 60)}}]`)
     ws.send('["REQ","meta",{"kinds":[0]}]')
+    ws.send('["REQ","follows",{"kinds":[3]}]')
   }
+}
+
+Number.prototype.pad = function(size) {
+    var s = String(this);
+    while (s.length < (size || 2)) {s = "0" + s;}
+    return s;
+}
+
+function timeFormat(ts) {
+  let d = new Date(ts * 1000)
+  return `${d.getYear()-100}-${(d.getMonth()+1).pad(2)}-${(d.getDate()+1).pad(2)} ` +
+         `${d.getHours().pad(2)}:${d.getMinutes().pad(2)}:${d.getSeconds().pad(2)}`
+}
+
+function escapeHTML(str){
+  var p = document.createElement("p");
+  p.appendChild(document.createTextNode(str));
+  return p.innerHTML;
+}
+
+function guessClient(event) {
+  if (event.tags.find(i => i[0] === 'client' && i[1] === 'more-speech')) {
+    return '<a href="https://github.com/unclebob/more-speech">more-speech</a>'
+  }
+  return null
 }
--- index.md
+++ index.md
@@ -4,7 +4,7 @@ layout: home
 
 <link rel="stylesheet" href="/assets/css/main.css">
 <script src="/assets/js/main.js"></script>
-
+<br>
 <label for="kind-filter">Filter by Kind:</label>
 <select name="kind-filter" id="kind-filter" onchange="dirty=true">
   <option value="all">kind-*: All kinds</option>
@@ -19,4 +19,17 @@ layout: home
   <option value="7">kind-7: Reactions</option>
   <option value="60">kind-60: Ride Sharing</option>
 </select>
+<br>
+<label for="pubkey-filter">Filter by Pubkey:</label>
+<input type="text" name="pubkey-filter" id="pubkey-filter" onchange="dirty=true">
+<label for="degree-filter">... and follows of degree:</label>
+<select name="degree-filter" id="degree-filter" disabled onchange="dirty=true">
+  <option value="0">Only the pubkey</option>
+  <option value="1">Follows of the pubkey</option>
+  <option value="2">Follows² of the pubkey</option>
+  <option value="3">Follows³ of the pubkey</option>
+  <option value="4">Follows⁴ of the pubkey</option>
+  <option value="5">Follows⁵ of the pubkey</option>
+</select>
+<br>
 <div id="output"></div>
