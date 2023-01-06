fd344cca78392a9f27537ad597c8c94a0b1dfbcf
Raul Piraces Alastuey <raul.piraces@gmail.com>
2023-01-06 12:19:30 +0100

    Add nostr developer.li relay
    Public nostr relay for general purpose I manage.

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,6 +27,7 @@ window.addEventListener('load', () => {
     'wss://nostr.zaprite.io',
     'wss://nostr-pub.semisol.dev',
     'wss://nostr.ono.re',
+    'wss://nostr.developer.li',
     // 'ws://jgqaglhautb4k6e6i2g34jakxiemqp6z4wynlirltuukgkft2xuglmqd.onion'
   ].map(it=>{ return {
     url: it,
